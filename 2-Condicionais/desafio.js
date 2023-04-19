b5bbf06d39cbfd08b12ec88dff4ac4879df2c0c7
Rafaella Massa <rafaella_massa@hotmail.com>
2023-04-19 13:49:15 -0300

    Conclusão da aula 2 e desafio

---
+++ 2-Condicionais/desafio.js
@@ -0,0 +1,24 @@
+/*Faça um programa para calcular o valor de uma viagem.
+Você terá 5 variáveis:
+- Preço do etanol;
+- Preço da gasolina;
+- Tipo de combustível no carro;
+- Gasto médio do carro por KM;
+- Distância em KM da viagem.
+*/
+
+const precoEtanol = 5.79;
+const precoGasolina = 6.66;
+const kmPorLitro = 10;
+const distancia = 100;
+let tipoCombustivel = 'Gasolina';
+
+const litrosConsumidos = distancia / kmPorLitro;
+
+if (tipoCombustivel === 'Etanol'){
+    const total = litrosConsumidos * precoEtanol;
+    console.log(total);
+} else {
+    const total = litrosConsumidos * precoGasolina;   
+    console.log(total) 
+}
