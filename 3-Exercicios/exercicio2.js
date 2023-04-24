56897342d6c9c79cfe102a41af552cb0d8432401
Rafaella Massa <rafaella_massa@hotmail.com>
2023-04-24 11:59:19 -0300

    Melhorando código do ex 2 com funções

--- 3-Exercicios/exercicio2.js
+++ 3-Exercicios/exercicio2.js
@@ -7,20 +7,28 @@ Acima de 40: obesidade grave.
 
 Fórmula do IMC: peso / (altura*altura)
 */
-
-const peso = 65;
-const altura = 1.55;
-
-const imc = (peso / Math.pow(altura, 2));
-console.log(imc.toFixed(2));
-if (imc < 18.5){
-    console.log("Abaixo do peso!");
-}else if (imc >= 18.5 && imc < 25){
-    console.log("Peso normal!")
-} else if (imc >= 25 && imc < 30){
-    console.log("Acima do peso!");
-}else if (imc >= 30 && imc < 40){
-    console.log("Obesidade!");
-} else {
-    console.log("Obesidade grave!")
+function calcularImc (peso, altura){
+    return peso / Math.pow(altura, 2)
 }
+
+function classificarImc (imc){
+    if (imc < 18.5){
+        return "Abaixo do peso!";
+    }else if (imc >= 18.5 && imc < 25){
+        return "Peso normal!";
+    } else if (imc >= 25 && imc < 30){
+        return "Acima do peso!";
+    }else if (imc >= 30 && imc < 40){
+        return "Obesidade!";
+    } else {
+        return "Obesidade grave!";
+    }
+}
+
+function main(){
+const imc = calcularImc(65, 1.55);
+console.log(imc.toFixed(2));
+console.log(classificarImc(imc));
+}
+
+main();
