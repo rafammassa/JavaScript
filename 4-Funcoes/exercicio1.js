6077ba9fc63a18bb94152ee11a20717ad44cc24f
Rafaella Massa <rafaella_massa@hotmail.com>
2023-04-24 13:37:17 -0300

    Exercício 1 de função

---
+++ 4-Funcoes/exercicio1.js
@@ -0,0 +1,13 @@
+function escrevaMeuNome (nome){
+    return 'Meu nome é '+ nome;
+}
+
+function verificarIdade(idade){
+    if(idade>=18){
+        console.log(escrevaMeuNome('Rafaella. ') + 'Maior de idade.')
+    } else {
+        console.log('Menor de idade.')
+    }
+}
+
+verificarIdade(18);
