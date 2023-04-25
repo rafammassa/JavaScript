0e304624471a517b4f1acca08968c9e9787211c7
Rafaella Massa <rafaella_massa@hotmail.com>
2023-04-25 15:56:07 -0300

    Exercicio Pares (de 10 a 50) com FOR

---
+++ 6-Arrays/pares10a50.js
@@ -0,0 +1,10 @@
+/*
+4) Faça um programa que preencha uma lista com 
+todos os números pares contidos no intervalo de 10 a 50;
+*/
+
+for (let i = 10; i <= 50; i++) {
+    if (i % 2 === 0){
+        console.log(i)
+    }
+}
